3bd9d90936a320fd8f7472503f78ece793f3af9e
GitLab <test@example.com>
2022-08-20 22:11:28 +0430

    add mongoose options

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,1 +1,8 @@
-// mongo
+import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+
+export const dbConnection = {
+  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  options: {
+    useNewUrlParser: true,
+  },
+};
