575f26e1845fad9999b4106036faeb68c63f6723
GitLab <test@example.com>
2022-08-20 22:12:34 +0430

    update project

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import App from './app';
+import ProductsRoute from './components/product/products.route';
+import UsersRoute from './components/users/users.route';
 
-const app = new App();
+const app = new App([new UsersRoute(), new ProductsRoute()]);
 
 app.start();
