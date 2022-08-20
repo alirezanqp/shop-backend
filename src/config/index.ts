17529fdd996aa750a726bced51adafc495111946
“alirezanqp” <alirezanqp@gmail.com>
2022-08-20 22:49:32 +0430

    fix: update .gitignore and src/config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
 
-config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { APP_PORT, APP_HOST, API_VERSION, NODE_ENV, DB_HOST, DB_PORT, DB_DATABASE, ORIGIN } = process.env;
