61cdc8fdcd4ccd2cf4b550d785df4fff9a470e67
alirezanqp <alirezanqp@gmail.com>
2022-08-30 13:44:30 +0430

    Revert "✨ feat: Add env validator"
    This reverts commit df494d8c74b29502c383fd9481bfc0f1b7c82f32.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import App from './app';
 import ProductsRoute from './components/product/products.route';
 import UsersRoute from './components/users/users.route';
-import validateEnv from '@/utils/validateEnv';
-
-validateEnv();
 
 const app = new App([new UsersRoute(), new ProductsRoute()]);
 
