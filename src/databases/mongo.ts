3ade30a7004114b7965c344c790df03e65d689cd
alirezanqp <alirezanqp@gmail.com>
2022-08-31 23:35:44 +0430

    🗃️ update mongodb options

--- src/databases/mongo.ts
+++ src/databases/mongo.ts
@@ -1,6 +1,6 @@
 import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
 
-export const dbConnection = {
+export const mongoDBConnection = {
   url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
   options: {
     useNewUrlParser: true,
