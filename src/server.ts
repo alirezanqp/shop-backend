d0a30302d4163f59fb62c738ae629b23127291ce
alirezanqp <alirezanqp@gmail.com>
2022-09-07 00:34:52 +0430

    feat: Add Auth Route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import App from './app';
+import AuthRoute from './components/auth/auth.route';
 import ProductsRoute from './components/product/products.route';
 import UsersRoute from './components/users/users.route';
 import validateEnv from './utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new UsersRoute(), new ProductsRoute()]);
+const app = new App([new UsersRoute(), new ProductsRoute(), new AuthRoute()]);
 
 app.start();
