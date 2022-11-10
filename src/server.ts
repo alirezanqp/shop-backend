0c8cd47eb74767bf215e6b4bdfa97b6b4f775735
alirezanqp <alirezanqp@gmail.com>
2022-11-10 15:17:09 +0330

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import App from './app';
 import AuthRoute from './components/auth/auth.route';
+import CategoryRoute from './components/category/category.route';
 import ProductsRoute from './components/product/products.route';
 import UsersRoute from './components/users/users.route';
 import validateEnv from './utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new UsersRoute(), new ProductsRoute(), new AuthRoute()]);
+const app = new App([new UsersRoute(), new ProductsRoute(), new AuthRoute(), new CategoryRoute()]);
 
 app.start();
