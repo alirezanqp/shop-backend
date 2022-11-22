75bd88c806836dd8141877b325ed724718dedfd0
alirezanqp <alirezanqp@gmail.com>
2022-11-22 23:45:53 +0330

    add LOG_DIR environment

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,4 +3,4 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { DATABASE_URL, SECRET_KEY, APP_PORT, APP_HOST, API_VERSION, NODE_ENV, ORIGIN, REDIS_URL } = process.env;
+export const { DATABASE_URL, SECRET_KEY, APP_PORT, APP_HOST, API_VERSION, NODE_ENV, ORIGIN, REDIS_URL, LOG_DIR } = process.env;
