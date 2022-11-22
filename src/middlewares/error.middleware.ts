b017eac4bb5e44875b11b08859cbac80e924bba7
alirezanqp <alirezanqp@gmail.com>
2022-11-22 23:44:13 +0330

    add logger to error middlware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,3 +1,4 @@
+import { logger } from '../utils/logger';
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '../exceptions/HttpExceoption';
 // import { logger } from '@utils/logger';
@@ -7,7 +8,7 @@ const errorMiddleware = (error: HttpException, req: Request, res: Response, next
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
 
-    // logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
+    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
     res.status(status).json({ message });
   } catch (error) {
     next(error);
